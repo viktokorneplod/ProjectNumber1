02c00bb6ec799f9489011c561f96ed6a2e75ebf6
ivan5228 <grigorichev_ivan@rambler.ru>
2025-02-20 23:13:45 +0300

    Update huf.cpp
    +Huffman Tree

--- huf.cpp
+++ huf.cpp
@@ -1,4 +1,6 @@
 #include "header.h"
+#include <fstream>
+#include <queue>
 #include <unordered_map>
 huffman_Node* create_Node(char symbol, int freq, huffman_Node* left, huffman_Node* right) {
     huffman_Node* node = new huffman_Node();
@@ -33,4 +35,60 @@ void deCode(huffman_Node* root, int& index, const std::string& inCode_string) {
     else {
         deCode(root->right, index, inCode_string);
     }
+}
+
+void build_Huffman_Tree(const std::string& inputFile, const std::string& outputFile) {
+
+    std::ifstream infile(inputFile);
+    if (!infile) {
+        std::cerr << "file reading error " << inputFile << std::endl;
+        return;
+    }
+
+    std::string text((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
+    infile.close();
+
+    std::unordered_map<char, int> freqMap;
+    for (char c : text) {
+        freqMap[c]++;
+    }
+
+    std::priority_queue<huffman_Node*, std::vector<huffman_Node*>, compareNode> queue_with_P;
+
+    for (auto& pair : freqMap) {
+        queue_with_P.push(create_Node(pair.first, pair.second, nullptr, nullptr));
+    }
+
+    while (queue_with_P.size() > 1) {
+        huffman_Node* left = queue_with_P.top(); queue_with_P.pop();
+        huffman_Node* right = queue_with_P.top(); queue_with_P.pop();
+        int sum = left->freq + right->freq;
+        queue_with_P.push(create_Node('\0', sum, left, right));
+    }
+
+    huffman_Node* root = queue_with_P.top();
+
+    std::unordered_map<char, std::string> huffmanCodes;
+    inCode(root, "", huffmanCodes);
+
+
+    std::ofstream outFile(outputFile);
+    if (!outFile) {
+        std::cerr << "file reading error: " << outputFile << std::endl;
+        return;
+    }
+
+    std::string encodedString;
+    for (char ch : text) {
+        encodedString += huffmanCodes[ch];
+    }
+
+    outFile << encodedString;
+    outFile.close();
+    std::cout << "output: ";
+    int index = -1;
+    while (index < (int)encodedString.size() - 2) {
+        deCode(root, index, encodedString);
+    }
+    std::cout << std::endl;
 }

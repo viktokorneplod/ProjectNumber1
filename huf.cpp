b5db2ce772b4060575cc7a76f05fe2cad3f123f3
VladimirPozdeev06 <vova0306pozdeev@yandex.ru>
2025-02-20 22:28:40 +0300

    Update huf.cpp

--- huf.cpp
+++ huf.cpp
@@ -10,4 +10,18 @@ void inCode(huffman_Node* root, std::string code, std::unordered_map<char, std::
     inCode(root->right, code + "1", huffman_Codes);
 }
 
+void deCode(huffman_Node* root, int& index, const std::string& inCode_string) {
+    if (root == nullptr) return;
 
+    if (!root->left && !root->right) {
+        std::cout << root->symbol;
+        return;
+    }
+    index--;
+    if (inCode_string[index] == '0') {
+        deCode(root->left, index, inCode_string);
+    }
+    else {
+        deCode(root->right, index, inCode_string);
+    }
+}

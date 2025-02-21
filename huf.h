34912dd12dcf942551b5fa96bea00ca632968806
ivan5228 <grigorichev_ivan@rambler.ru>
2025-02-21 08:44:19 +0300

    Update huf.h

--- huf.h
+++ huf.h
@@ -7,10 +7,10 @@
 struct huffman_Node {
     char symbol;
     int freq;
-    huffman_Node *left, *right;
+    huffman_Node* left, * right;
 };
 huffman_Node* create_Node(char symbol, int freq, huffman_Node* left, huffman_Node* right);
 struct compareNode {
     bool operator()(huffman_Node* left, huffman_Node* right);
 };
-void build_Huffman_Tree(const std::string& inputFile, const std::string& outputFile);
+void build_Huffman_Tree(const std::string& inputFile, const std::string& outputFile, const std::string& decodedOutputFile);
